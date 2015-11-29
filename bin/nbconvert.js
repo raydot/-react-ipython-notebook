5859a5724bc9b2601b9587247fecff5fdfb45f3f
Ricky Reusser <rsreusser@gmail.com>
2015-11-29 10:37:18 -0500

    Rebuild

--- bin/nbconvert.js
+++ bin/nbconvert.js
@@ -10,6 +10,10 @@ var _process = require('process');
 
 var _process2 = _interopRequireDefault(_process);
 
+var _victorica = require('victorica');
+
+var _victorica2 = _interopRequireDefault(_victorica);
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -22,13 +26,11 @@ var _reactIpythonNotebook = require('react-ipython-notebook');
 
 var _reactIpythonNotebook2 = _interopRequireDefault(_reactIpythonNotebook);
 
-var _victorica = require('victorica');
-
-var _victorica2 = _interopRequireDefault(_victorica);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var input = JSON.parse(_fs2.default.readFileSync(_process2.default.argv[2]));
+
+// Symlinked to ../lib for development:
 var output = _server2.default.renderToStaticMarkup(_react2.default.createElement(_reactIpythonNotebook2.default, { data: input }));
 var beautifiedOutput = (0, _victorica2.default)(output);
 
